67d11c11876988ff6b576bcea966fac602df3572
Amir <amir.khalidov@gmail.com>
2024-11-16 22:59:18 +0400

    first meaasge

---
+++ tailwind.config.js
@@ -0,0 +1,31 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+    content: ['./src/**/*.{html,js}'],
+    theme: {
+        extend: {
+            colors: {
+                primary: {
+                    darkBlue: 'hsl(243, 87%, 12%)',
+                    desaturatedBlue: 'hsl(238, 22%, 44%)',
+                },
+                accent: {
+                    brightBlue: 'hsl(224, 93%, 58%)',
+                    moderateCyan: 'hsl(170, 45%, 43%)',
+                },
+                neutral: {
+                    lightGrayishBlue: 'hsl(240, 75%, 98%)',
+                    lightGray: 'hsl(0, 0%, 75%)',
+                },
+            },
+            fontFamily: {
+                raleway: ['Raleway', 'sans-serif'],
+                openSans: ['Open Sans', 'sans-serif'],
+            },
+            screens: {
+                md: '830px',
+                lg: '900px',
+            },
+        },
+    },
+    plugins: [],
+};
